0caded51f7af4d02f03214177c0b229d3f218ee8
stanley <ou.stanley@yahoo.com>
2022-08-14 09:38:44 -0400

    Added eager loading to include count of numMembers of Memberships table.

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,13 +1,25 @@
 const router = require("express").Router();
+const sequelize = require("sequelize");
 const { validateLogin } = require("../../utils/validation");
 const { setTokenCookie } = require("../../utils/auth");
 const { Group, Membership } = require("../../db/models");
 
 // Get all groups
 router.get("/", async (req, res, next) => {
-  const Groups = await Group.findAll({ include: { model: Membership } });
+  const allGroups = await Group.findAll({
+    attributes: {
+      include: [
+        [sequelize.fn("COUNT", sequelize.col("Memberships.id")), "numMembers"],
+      ],
+    },
+    group: "name",
+    include: {
+      model: Membership,
+      attributes: [],
+    },
+  });
 
-  res.json({ Groups });
+  res.json({ Groups: allGroups });
 });
 
 module.exports = router;
