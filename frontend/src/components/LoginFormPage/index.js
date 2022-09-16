5bebf409a3fef8e97dc4cebe2bb0da7d97c6a670
stanley <ou.stanley@yahoo.com>
2022-09-15 21:43:43 -0400

    Styled LoginForm.

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { login } from "../../store/session";
+import "./LoginForm.css";
 
 const LoginFormPage = () => {
   const dispatch = useDispatch();
@@ -9,6 +10,7 @@ const LoginFormPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [wiggle, setWiggle] = useState(false);
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -30,28 +32,36 @@ const LoginFormPage = () => {
   return (
     <section className="login">
       <form className="login-form" onSubmit={submitHandler}>
-        {errors.status && <h3>Login failed: {errors.message}</h3>}
-        <label>
-          Email
-          <input
-            type="text"
-            placeholder="Email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <button type="submit">Log In</button>
+        <div className="errors">
+          {errors.status && (
+            <h3
+              key={Math.random() * 100}
+              onAnimationEnd={() => setWiggle(false)}
+              className={wiggle ? "wiggle" : ""}
+            >
+              Login failed: {errors.message}
+            </h3>
+          )}
+        </div>
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={() => setWiggle(true)}>
+          Log In
+        </button>
       </form>
     </section>
   );
