aac16731255dfb0e4b8bb5bf807168af281f6c8b
stanley <ou.stanley@yahoo.com>
2022-09-30 12:00:29 -0400

    Updated CSS styling.

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,24 +1,29 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import LoginSignupModal from "../LoginSignupModal";
 
 const Navigation = () => {
+  const { pathname } = useLocation();
   const activeSession = useSelector((state) => state.session.user);
 
   return (
     <nav className="navbar">
       <ul>
         <li>
-          <Link className="navlink" to="/">
+          <Link to="/">
             {/* <i className="fa-solid fa-house fa-2x" /> */}
             <span id="home-logo">RendeVue</span>
           </Link>
         </li>
         <li className="login-signup-li">
-          <Link to="/groups">Groups</Link>
-          <Link to="/events">Events</Link>
+          <Link className={pathname === "/groups" ? "active" : ""} to="/groups">
+            Groups
+          </Link>
+          <Link className={pathname === "/events" ? "active" : ""} to="/events">
+            Events
+          </Link>
           {!activeSession ? (
             <LoginSignupModal />
           ) : (
